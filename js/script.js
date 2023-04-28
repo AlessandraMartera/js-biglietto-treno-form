262cca1f19f44195d9d0db5ae1355b95c4a1fd48
alessandra martera <ale.martera@gmail.com>
2023-04-28 16:35:26 +0200

    Funtion clean

--- js/script.js
+++ js/script.js
@@ -3,27 +3,64 @@ let userName, userAge, userKm, ticketPrice, discountP;
 let messageDiscount = "hai diritto allo sconto";
 const kmPrice = 0.21;
 const message = "il prezzo del tuo biglitto è"
-
-userName = document.getElementById("user_name").value;
-userKm = document.getElementById("user_km").value;
-userAge = document.getElementById("user_age").value;
-
-// build an input link
 const crea = document.getElementById("invio");
-// FUNCTION
-crea.addEventListener ( "click", 
+const clean = document.getElementById("clean");
 
-function() {
+function myFunction () {
+  // take user data
+  userName = document.getElementById("user_name").value;
+  userKm = document.getElementById("user_km").value;
+  userAge = document.getElementById("user_age").value;
   document.getElementById("demo").innerHTML = userName + userKm + userAge;
+
+  // calcola il prezzo del biglietto a seconda di qualti km deve fare
+  ticketPrice = userKm * kmPrice;
+  console.log("Ticket Price is " + ticketPrice + " euro");
+  
+  // stampa il prezzo del biglietto
+  document.getElementById("price").innerHTML = ticketPrice;
+  
+  // controlla gli anni dell'utente 
+  // IF discount 20%
+  if (userAge<18)  {
+    discountP = "20%";
+    discount = ticketPrice * 0.20;
+    ticketPrice = ticketPrice - discount;
+  }
+
+  // ELSE IF discount 40%
+  else if(userAge>=65)  {
+      discountP = "40%";
+      discount = ticketPrice * 0.40;
+      ticketPrice = ticketPrice - discount;
+  }
+
+  // ELSE nothing discount 
+  else {
+      messageDiscount = "non hai diritto a sconti"
+      discountP = "";
+  } 
+
 }
-);
 
-// calcola il prezzo del biglietto a seconda di qualti km deve fare
-ticketPrice = userKm * kmPrice;
-console.log("Ticket Price is " + ticketPrice + " euro");
+// start button
+crea.addEventListener ( "click", myFunction);
 
-// stampa il prezzo del biglietto
-document.getElementById("price").innerHTML = ticketPrice;
+
+// clean button
+clean.addEventListener ( "click", 
+  function() {
+    document.getElementById("user_name").value = "";
+    document.getElementById("user_km").value = "";
+    document.getElementById("user_age").value = "";
+
+  }
+)
+
+
+
+
+
 
 // stampa all'utente se ha diritto a degli sconti e nel caso far vedere quando deve pagare
 
