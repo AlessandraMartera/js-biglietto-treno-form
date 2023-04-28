5e3437a206b913ad508a71ac37aa19dcab5eb708
alessandra martera <ale.martera@gmail.com>
2023-04-28 18:16:07 +0200

    Stamp base ticket

--- js/script.js
+++ js/script.js
@@ -1,54 +1,54 @@
 // startig Var
-let userName, userAge, userKm, ticketPrice, discountP;
-let messageDiscount = "hai diritto allo sconto";
-const kmPrice = 0.21;
-const message = "il prezzo del tuo biglitto è"
-const crea = document.getElementById("invio");
-const clean = document.getElementById("clean");
+let userName, userSurname, userAge, userKm, ticketPrice, discountP;
 
-function myFunction () {
+const kmPrice = 0.21;
+const stampTicket= document.getElementById("generate");
+const cleanInput = document.getElementById("clean");
+
+function calcTicketPrice () {
   // take user data
+  ticketPrice = "";
   userName = document.getElementById("user_name").value;
+  userSurname = document.getElementById("user_Surname").value;
   userKm = document.getElementById("user_km").value;
   userAge = document.getElementById("user_age").value;
-  document.getElementById("demo").innerHTML = userName + userKm + userAge;
+  document.getElementById("data").innerHTML = `${userName} ${userSurname}`;
 
-  // calcola il prezzo del biglietto a seconda di qualti km deve fare
+  // calc ticket price from km to do
   ticketPrice = userKm * kmPrice;
   console.log("Ticket Price is " + ticketPrice + " euro");
   
-  // stampa il prezzo del biglietto
-  document.getElementById("price").innerHTML = ticketPrice;
-  
-  // controlla gli anni dell'utente 
+  //check user age
   // IF discount 20%
   if (userAge<18)  {
-    discountP = "20%";
     discount = ticketPrice * 0.20;
     ticketPrice = ticketPrice - discount;
   }
 
   // ELSE IF discount 40%
   else if(userAge>=65)  {
-      discountP = "40%";
       discount = ticketPrice * 0.40;
       ticketPrice = ticketPrice - discount;
   }
 
   // ELSE nothing discount 
   else {
-      messageDiscount = "non hai diritto a sconti"
-      discountP = "";
+      discount = 0;
+      ticketPrice = ticketPrice - discount;
   } 
 
+  // stamp ticket price
+  document.getElementById("price").innerHTML = ticketPrice.toFixed(2) + "€";
+
+  
 }
 
-// start button
-crea.addEventListener ( "click", myFunction);
+// Generate button
+stampTicket.addEventListener ( "click", calcTicketPrice );
 
 
 // clean button
-clean.addEventListener ( "click", 
+cleanInput.addEventListener ( "click", 
   function() {
     document.getElementById("user_name").value = "";
     document.getElementById("user_km").value = "";
@@ -56,40 +56,5 @@ clean.addEventListener ( "click",
 
   }
 )
-
-
-
-
-
-
-// stampa all'utente se ha diritto a degli sconti e nel caso far vedere quando deve pagare
-
-// controlla gli anni dell'utente 
-// IF discount 20%
-if (userAge<18)  {
-  discountP = "20%";
-  discount = ticketPrice * 0.20;
-  ticketPrice = ticketPrice - discount;
-}
-
-// ELSE IF discount 40%
-else if(userAge>=65)  {
-    discountP = "40%";
-    discount = ticketPrice * 0.40;
-    ticketPrice = ticketPrice - discount;
-}
-
-// ELSE nothing discount 
-else {
-    messageDiscount = "non hai diritto a sconti"
-    discountP = "";
-} 
-
-// Stamp massege discount
-// document.getElementById("discount").innerHTML = `${messageDiscount} ${discountP}`;
-
-// stamp new price
-// document.getElementById("discount_price").innerHTML = "il prezzo del biglietto è " +  ticketPrice.toFixed(2) + " euro";
-
 
 
